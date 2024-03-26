c4618a8badd4e990c6840e7029ca3fb01bc87d90
Sebastian Davrieux <s.davrieux@amsterdam.nl>
2024-03-26 10:26:48 +0100

    Logging to file:

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 bash objectherkenning_openbare_ruimte/preprocessing_service/preprocess_video_to_frames.sh &
 
-source /venv/bin/activate
-PYTHONPATH=. python -u objectherkenning_openbare_ruimte/data_delivery_pipeline/run_data_delivery_pipeline.py &
+#source /venv/bin/activate
+#PYTHONPATH=. python -u objectherkenning_openbare_ruimte/data_delivery_pipeline/run_data_delivery_pipeline.py &
 
 tail -F /dev/null
--- objectherkenning_openbare_ruimte/preprocessing_service/preprocess_video_to_frames.sh
+++ objectherkenning_openbare_ruimte/preprocessing_service/preprocess_video_to_frames.sh
@@ -4,8 +4,11 @@ VIDEOS_FOLDER="/raw_videos"
 OUTPUT_FOLDER="/raw_frames"
 
 while true; do
+  TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+  LOG_FILE="log_$TIMESTAMP.txt"
   printf "%s\n" $VIDEOS_FOLDER
   for video_file in "$VIDEOS_FOLDER"/*.mp4; do
+    printf "%s\n" "$video_file" >> $LOG_FILE
     printf "%s\n" "$video_file"
     if [ -f "$video_file" ]; then
         file_name=$(basename "$video_file")
@@ -14,8 +17,11 @@ while true; do
         RESULT=$?
         if [ $RESULT -eq 0 ]; then
           rm "$video_file"
+          printf "ffmpeg completed, file removed: %s\n" "$video_file"
+          printf "ffmpeg completed, file removed: %s\n" "$video_file" >> $LOG_FILE
         else
           printf "Error running ffmpeg on: %s\n" "$video_file"
+          printf "Error running ffmpeg on: %s\n" "$video_file" >> $LOG_FILE
         fi
     fi
   done
