395898b0111f771b6464c7e7b6db911c0b431954
imandrealombardo <60581994+imandrealombardo@users.noreply.github.com>
2024-03-27 22:16:40 +0100

    first version of training pipeline

---
+++ objectherkenning_openbare_ruimte/training_pipeline/submit_training_pipeline.py
@@ -0,0 +1,57 @@
+from aml_interface.azure_logging import setup_azure_logging  # noqa: E402
+from azure.ai.ml import Input, Output
+from azure.ai.ml.constants import AssetTypes
+from azure.ai.ml.dsl import pipeline
+
+from objectherkenning_openbare_ruimte.settings.settings import (
+    ObjectherkenningOpenbareRuimteSettings,
+)
+from objectherkenning_openbare_ruimte.training_pipeline.components.train_model import (
+    train_model,
+)
+
+ObjectherkenningOpenbareRuimteSettings.set_from_yaml("config.yml")
+settings = ObjectherkenningOpenbareRuimteSettings.get_settings()
+setup_azure_logging(settings["logging"], __name__)
+
+from aml_interface.aml_interface import AMLInterface  # noqa: E402
+
+
+@pipeline()
+def training_pipeline():
+
+    project_path_name = settings["training_pipeline"]["outputs"]["project_path"]
+
+    training_data = Input(
+        type=AssetTypes.URI_FOLDER,
+        path=settings["training_pipeline"]["inputs"]["training_data"],
+    )
+    model_weights = Input(
+        type=AssetTypes.URI_FOLDER,
+        path=settings["training_pipeline"]["inputs"]["model_weights"],
+    )
+    train_model_step = train_model(
+        mounted_dataset=training_data, model_weights=model_weights
+    )
+    train_model_step.outputs.yolo_yaml_path = Output(
+        type="uri_folder", mode="rw_mount", path=model_weights.path
+    )
+
+    project_path = aml_interface.get_datastore_full_path(project_path_name)
+    train_model_step.outputs.project_path = Output(
+        type="uri_folder", mode="rw_mount", path=project_path
+    )
+
+    return {}
+
+
+if __name__ == "__main__":
+    # Retrieve values from the YAML
+    ObjectherkenningOpenbareRuimteSettings.set_from_yaml("config.yml")
+    settings = ObjectherkenningOpenbareRuimteSettings.get_settings()
+
+    default_compute = settings["aml_experiment_details"]["compute_name"]
+    aml_interface = AMLInterface()
+    aml_interface.submit_pipeline_experiment(
+        training_pipeline, "training_pipeline", default_compute
+    )
