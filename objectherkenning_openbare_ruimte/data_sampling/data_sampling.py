6232b2ab324f49b7e1d56eb93707af0400f4a713
daanbl <daan.bloembergen@gmail.com>
2024-04-17 16:49:57 +0200

    Objectify data sampling code

--- objectherkenning_openbare_ruimte/data_sampling/data_sampling.py
+++ objectherkenning_openbare_ruimte/data_sampling/data_sampling.py
@@ -28,151 +28,195 @@ RD_CRS = "EPSG:28992"
 LAT_LON_CRS = "EPSG:4326"
 
 
-def load_metadata_gdf(path: str) -> gpd.GeoDataFrame:
-    meta_df = pd.read_csv(path)
-    return gpd.GeoDataFrame(
-        meta_df,
-        geometry=gpd.points_from_xy(
-            x=meta_df.gps_lon,
-            y=meta_df.gps_lat,
-            crs=LAT_LON_CRS,
-        ),
-    ).to_crs(RD_CRS)
+class DataSampling:
 
+    @staticmethod
+    def load_metadata_gdf(path: str) -> gpd.GeoDataFrame:
+        """
+        Load a CSV metadata file as GeoDataFrame. Assumes geometry information is
+        stored in columns named gps_lon and gps_lat. CRS will be converted to Rijksdriehoek.
 
-def _add_metadata_and_permits(
-    frame_gdf: gpd.GeoDataFrame,
-    metadata_folder: str,
-    decos_gdf: gpd.GeoDataFrame,
-    decos_buffer: float,
-) -> gpd.GeoDataFrame:
-    for src_name in frame_gdf["frame_src"].unique():
-        print(f"Adding metadata for {src_name}")
-        idx = np.where(frame_gdf["frame_src"] == src_name)[0]
-        meta_key_split = src_name.split(sep="-", maxsplit=2)
-        meta_file = f"{meta_key_split[0]}-{meta_key_split[2]}.csv"
-        meta_path = os.path.join(metadata_folder, meta_file)
+        Parameters
+        ----------
+        path: str
+            Path to metadata SCV file
 
-        meta_gdf = load_metadata_gdf(meta_path).set_index("new_frame_id")["geometry"]
+        Returns
+        -------
+        GeoDataFrame containing the metadata
+        """
+        meta_df = pd.read_csv(path)
+        return gpd.GeoDataFrame(
+            meta_df,
+            geometry=gpd.points_from_xy(
+                x=meta_df.gps_lon,
+                y=meta_df.gps_lat,
+                crs=LAT_LON_CRS,
+            ),
+        ).to_crs(RD_CRS)
 
-        geoms = meta_gdf.to_list()
-        if len(idx) > len(geoms):
-            geoms.extend([None] * (len(idx) - len(geoms)))
-        elif len(geoms) > len(idx):
-            geoms = geoms[0 : len(idx)]
+    @staticmethod
+    def add_metadata_and_permits(
+        frame_gdf: gpd.GeoDataFrame,
+        metadata_folder: str,
+        decos_gdf: gpd.GeoDataFrame,
+        decos_buffer: float,
+    ) -> gpd.GeoDataFrame:
+        """
+        Add metadata and permit information for each image frame in frame_gdf.
 
-        frame_gdf.loc[idx, "geometry"] = geoms
+        The frame_gdf is expected to contain one line for each frame, and a column
+        "frame_src" indicating the video the frame originated from. This will be
+        used as key to locate the right metadata file.
 
-        decos_bb = sg.box(*frame_gdf.loc[idx, :].total_bounds)
-        date = datetime.strptime(meta_file.split(sep="-")[1], "D%dM%mY%Y")
-        decos_gdf_filtered = decos_helper.filter_decos_by_date(decos_gdf, date)
-        decos_clipped = decos_gdf_filtered.clip(decos_bb.buffer(decos_buffer))
-        if len(decos_clipped) >= 1:
-            decos_area = decos_clipped.unary_union.buffer(decos_buffer)
-            permits = frame_gdf.loc[idx, :].within(decos_area).to_numpy()
-            frame_gdf.loc[idx, "permit"] = permits
+        The permit data TODO
 
-        print(f"{np.count_nonzero(permits)}/{len(permits)} frames in permit zone")
+        Parameters
+        ----------
+        frame_gdf: GeoDataFrame
+            TODO
 
-    return frame_gdf
+        Returns
+        -------
+        GeoDataFrame containing the metadata
+        """
+        for src_name in frame_gdf["frame_src"].unique():
+            print(f"Adding metadata for {src_name}")
+            idx = np.where(frame_gdf["frame_src"] == src_name)[0]
+            meta_key_split = src_name.split(sep="-", maxsplit=2)
+            meta_file = f"{meta_key_split[0]}-{meta_key_split[2]}.csv"
+            meta_path = os.path.join(metadata_folder, meta_file)
 
+            meta_gdf = DataSampling.load_metadata_gdf(meta_path).set_index(
+                "new_frame_id"
+            )["geometry"]
 
-def load_frame_gdf(
-    input_folder: str,
-    metadata_folder: str,
-    decos_gdf: gpd.GeoDataFrame,
-    decos_buffer: float,
-) -> gpd.GeoDataFrame:
-    frames = sorted(list(pathlib.Path(input_folder).glob("*.jpg")))
+            geoms = meta_gdf.to_list()
+            if len(idx) > len(geoms):
+                geoms.extend([None] * (len(idx) - len(geoms)))
+            elif len(geoms) > len(idx):
+                geoms = geoms[0 : len(idx)]
 
-    frame_data = {
-        "path": frames,
-        "frame_src": [
-            frame.name.replace(".jpg", "").rsplit(sep="_", maxsplit=1)[0]
-            for frame in frames
-        ],
-        "frame_id": [
-            int(frame.name.replace(".jpg", "").rsplit(sep="_", maxsplit=1)[1])
-            for frame in frames
-        ],
-        "geometry": np.empty_like(frames, dtype=object),
-        "permit": np.zeros_like(frames, dtype=bool),
-        "sample": np.zeros_like(frames, dtype=bool),
-    }
+            frame_gdf.loc[idx, "geometry"] = geoms
 
-    frame_gdf = gpd.GeoDataFrame(data=frame_data, crs=RD_CRS)
+            decos_bb = sg.box(*frame_gdf.loc[idx, :].total_bounds)
+            date = datetime.strptime(meta_file.split(sep="-")[1], "D%dM%mY%Y")
+            decos_gdf_filtered = decos_helper.filter_decos_by_date(decos_gdf, date)
+            decos_clipped = decos_gdf_filtered.clip(decos_bb.buffer(decos_buffer))
+            if len(decos_clipped) >= 1:
+                decos_area = decos_clipped.unary_union.buffer(decos_buffer)
+                permits = frame_gdf.loc[idx, :].within(decos_area).to_numpy()
+                frame_gdf.loc[idx, "permit"] = permits
 
-    print(f"{len(frame_gdf)} frames found")
+            print(f"{np.count_nonzero(permits)}/{len(permits)} frames in permit zone")
 
-    return _add_metadata_and_permits(
-        frame_gdf, metadata_folder, decos_gdf, decos_buffer
-    )
+        return frame_gdf
 
+    @staticmethod
+    def load_frame_gdf(
+        input_folder: str,
+        metadata_folder: str,
+        decos_gdf: gpd.GeoDataFrame,
+        decos_buffer: float,
+    ) -> gpd.GeoDataFrame:
+        frames = sorted(list(pathlib.Path(input_folder).glob("*.jpg")))
 
-def sample_frames(
-    frame_gdf: gpd.GeoDataFrame, n_frames: int, sampling_weight: float
-) -> gpd.GeoDataFrame:
-    weights = np.array(
-        [
-            sampling_weight if permit else 1 - sampling_weight
-            for permit in frame_gdf["permit"]
-        ]
-    )
-    weights = weights / np.sum(weights)
+        frame_data = {
+            "path": frames,
+            "frame_src": [
+                frame.name.replace(".jpg", "").rsplit(sep="_", maxsplit=1)[0]
+                for frame in frames
+            ],
+            "frame_id": [
+                int(frame.name.replace(".jpg", "").rsplit(sep="_", maxsplit=1)[1])
+                for frame in frames
+            ],
+            "geometry": np.empty_like(frames, dtype=object),
+            "permit": np.zeros_like(frames, dtype=bool),
+            "sample": np.zeros_like(frames, dtype=bool),
+        }
 
-    if n_frames >= np.count_nonzero(weights):
-        draw = np.where(weights > 0)[0]
+        frame_gdf = gpd.GeoDataFrame(data=frame_data, crs=RD_CRS)
+
+        print(f"{len(frame_gdf)} frames found")
+
+        return DataSampling.add_metadata_and_permits(
+            frame_gdf, metadata_folder, decos_gdf, decos_buffer
+        )
+
+    def __init__(
+        self,
+        input_folder: str,
+        metadata_folder: str,
+        decos_folder: str,
+        output_folder: str,
+    ):
+        self.output_folder = output_folder
+        self.n_frames = settings["data_sampling"]["n_frames"]
+        self.sampling_weight = settings["data_sampling"]["sampling_weight"]
+
+        decos_buffer = settings["data_sampling"]["decos_buffer"]
+        decos_gdf = decos_helper.load_and_combine_decos(decos_folder)
+        self.frame_gdf = DataSampling.load_frame_gdf(
+            input_folder, metadata_folder, decos_gdf, decos_buffer
+        )
+
+    def sample_frames(self):
+        weights = np.array(
+            [
+                self.sampling_weight if permit else 1 - self.sampling_weight
+                for permit in self.frame_gdf["permit"]
+            ]
+        )
+        weights = weights / np.sum(weights)
+
+        if self.n_frames >= np.count_nonzero(weights):
+            draw = np.where(weights > 0)[0]
+            print(
+                f"Cannot draw {self.n_frames} samples out of {np.count_nonzero(weights)} nonzero weights, sampling all nonzero."
+            )
+        else:
+            draw = np.random.choice(
+                np.arange(len(self.frame_gdf)), self.n_frames, p=weights, replace=False
+            )
+        sample = np.zeros_like(self.frame_gdf["permit"], dtype=bool)
+        sample[draw] = True
+        self.frame_gdf["sample"] = sample
+
+    def copy_sample_to_output_folder(self):
+        pathlib.Path(self.output_folder).mkdir(exist_ok=True, parents=True)
+
+        print(f"Copying sample to {self.output_folder}")
+
+        for i, row in self.frame_gdf.iterrows():
+            if row["sample"]:
+                frame = row["path"]
+                target_path = os.path.join(self.output_folder, frame.name)
+                shutil.copyfile(frame.as_posix(), target_path)
+
+    def store_metadata(self):
+        metadata_file = os.path.join(self.output_folder, "metadata.gpkg")
+        metadata_gdf = self.frame_gdf.assign(
+            path=self.frame_gdf["path"].apply(lambda p: p.as_posix())
+        )
+        print(f"Storing metadata in {metadata_file}")
+        metadata_gdf.to_file(metadata_file, driver="GPKG")
+
+    def run_sampling_job(self):
+
+        self.sample_frames()
+
+        n_sampled = np.count_nonzero(self.frame_gdf["sample"])
+        n_with_permit = np.count_nonzero(
+            self.frame_gdf["sample"] & self.frame_gdf["permit"]
+        )
+        ratio = (n_with_permit / n_sampled) * 100
         print(
-            f"Cannot draw {n_frames} samples out of {np.count_nonzero(weights)} nonzero weights, sampling all nonzero."
+            f"Sampled {n_sampled} frames, of which {n_with_permit} in permit zone ({ratio}%)"
         )
-    else:
-        draw = np.random.choice(
-            np.arange(len(frame_gdf)), n_frames, p=weights, replace=False
-        )
-    sample = np.zeros_like(frame_gdf["permit"], dtype=bool)
-    sample[draw] = True
-    frame_gdf["sample"] = sample
 
-    return frame_gdf
-
-
-def copy_sample(frame_gdf: gpd.GeoDataFrame, output_folder: str):
-    pathlib.Path(output_folder).mkdir(exist_ok=True, parents=True)
-
-    print(f"Copying sample to {output_folder}")
-
-    for i, row in frame_gdf.iterrows():
-        if row["sample"]:
-            frame = row["path"]
-            target_path = os.path.join(output_folder, frame.name)
-            shutil.copyfile(frame.as_posix(), target_path)
-
-
-def run_sampling_job(
-    input_folder: str, metadata_folder: str, decos_folder: str, output_folder: str
-):
-    ds_settings = settings["data_sampling"]
-    n_frames = ds_settings["n_frames"]
-    sampling_weight = ds_settings["sampling_weight"]
-    decos_buffer = ds_settings["decos_buffer"]
-
-    decos_gdf = decos_helper.load_and_combine_decos(decos_folder)
-    frame_gdf = load_frame_gdf(input_folder, metadata_folder, decos_gdf, decos_buffer)
-    frame_gdf = sample_frames(frame_gdf, n_frames, sampling_weight)
-
-    n_sampled = np.count_nonzero(frame_gdf["sample"])
-    n_with_permit = np.count_nonzero(frame_gdf["sample"] & frame_gdf["permit"])
-    ratio = (n_with_permit / n_sampled) * 100
-    print(
-        f"Sampled {n_sampled} frames, of which {n_with_permit} in permit zone ({ratio}%)"
-    )
-
-    copy_sample(frame_gdf, output_folder)
-
-    metadata_file = os.path.join(output_folder, "metadata.gpkg")
-    frame_gdf = frame_gdf.assign(path=frame_gdf["path"].apply(lambda p: p.as_posix()))
-    print(f"Storing metadata in {metadata_file}")
-    frame_gdf.to_file(metadata_file, driver="GPKG")
+        self.copy_sample_to_output_folder()
+        self.store_metadata()
 
 
 def parse_args():
@@ -194,6 +238,7 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    run_sampling_job(
+    sampler = DataSampling(
         args.input_folder, args.metadata_folder, args.decos_folder, args.output_folder
     )
+    sampler.run_sampling_job()
