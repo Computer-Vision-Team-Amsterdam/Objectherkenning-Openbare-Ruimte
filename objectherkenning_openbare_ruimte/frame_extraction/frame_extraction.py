085bc2ffbae451fe842c9d775ae9e2b8c2f67d49
daanbl <daan.bloembergen@gmail.com>
2024-04-19 17:23:11 +0200

    Better handling of ffmpeg command, more docstring

--- objectherkenning_openbare_ruimte/frame_extraction/frame_extraction.py
+++ objectherkenning_openbare_ruimte/frame_extraction/frame_extraction.py
@@ -3,6 +3,9 @@ import os
 import pathlib
 import sys
 from datetime import datetime
+from subprocess import CalledProcessError, check_call  # nosec: B404
+
+from pathvalidate import is_valid_filepath
 
 sys.path.append(os.getcwd())
 
@@ -18,11 +21,96 @@ ObjectherkenningOpenbareRuimteSettings.set_from_yaml(config_path)
 settings = ObjectherkenningOpenbareRuimteSettings.get_settings()
 
 
+class FFMPEG_frame_extractor:
+    """
+    This class wraps around the ffmpeg command to facilitate frame extraction and
+    distortion correction from videos.
+
+    Checks are in place to ensure arguments are valid:
+    fps, cx, cy, k1, k2 must be float
+    input_file must be an existing file
+    output_folder must be an existing folder
+    log_file must point to a valid filename in an existing folder
+    """
+
+    def __init__(self, fps, distortion_settings):
+        self.cmd = None
+
+        cx = distortion_settings["cx"]
+        cy = distortion_settings["cy"]
+        k1 = distortion_settings["k1"]
+        k2 = distortion_settings["k2"]
+
+        if not isinstance(fps, float):
+            print(f"Invalid type for fps: {type(fps)}")
+            return
+        if not isinstance(cx, float):
+            print(f"Invalid type for fps: {type(fps)}")
+            return
+        if not isinstance(cy, float):
+            print(f"Invalid type for fps: {type(fps)}")
+            return
+        if not isinstance(k1, float):
+            print(f"Invalid type for fps: {type(fps)}")
+            return
+        if not isinstance(k2, float):
+            print(f"Invalid type for fps: {type(fps)}")
+            return
+
+        # Prepare FFMPEG command for distortion correction and output quality
+        self.filter_str = (
+            f"fps={fps},lenscorrection=cx={cx}:cy={cy}:k1={k1}:k2={k2}:i=bilinear"
+        )
+
+    def create_command(self, input_file: str, output_path: str, log_file: str) -> str:
+        if not os.path.isfile(input_file):
+            print(f"Input file not found: {input_file}")
+            return None
+        if not os.path.isdir(os.path.dirname(output_path)):
+            print(f"Output folder not found: {os.path.dirname(output_path)}")
+            return None
+        if not is_valid_filepath(output_path):
+            print(f"Output file path not valid: {output_path}")
+            return None
+        if not os.path.isdir(os.path.dirname(log_file)):
+            print(f"Log folder not found: {os.path.dirname(log_file)}")
+            return None
+        if not is_valid_filepath(log_file):
+            print(f"Log file path not valid: {log_file}")
+            return None
+
+        self.cmd = [
+            "ffmpeg",
+            "-i",
+            input_file,
+            "-vf",
+            f"'{self.filter_str}'",
+            "-q:v",
+            "1",  # [-q:v 1] sets output jpg quality to max
+            f"{output_path}_%04d.jpg",
+            "2>",
+            log_file,
+        ]
+        return " ".join(self.cmd)
+
+    def run(self):
+        if self.cmd is None:
+            print("Command not set, run 'create_cmd() first.")
+        try:
+            check_call(self.cmd)  # nosec: B603
+        except CalledProcessError as e:
+            print(e.output)
+
+
 def main(input_folder: str, output_folder: str):
     """
-    Recursively iterates through all MP4 files in the input_folder and its subfolders, extracts frames at a given framerate, and applies distortion correction.
+    Recursively iterates through all MP4 files in the input_folder and its subfolders,
+    extracts frames at a given framerate, and applies distortion correction.
 
-    All frames are extracted to the same folder, with their names reflecting the name of the source video file. In a separate folder log files will be written, one general with the executed ffmpeg commands, and individual log files for each ffmpeg operation.
+    All frames are extracted to the same folder, with their names reflecting the name
+    of the source video file. In a separate folder log files will be written, one
+    general with the executed ffmpeg commands, and individual log files for each
+    ffmpeg operation.
 
     Usage:
     python frame_extraction.py --input_folder <INPUT> --output_folder <OUTPUT>
@@ -37,24 +125,20 @@ def main(input_folder: str, output_folder: str):
     output_folder: str
         Folder to which extracted frames and log files will be written.
     """
-    fe_settings = settings["frame_extraction"]
-    dc_settings = settings["distortion_correction"]
+    exclude_dirs = set(settings["frame_extraction"]["exclude_dirs"])
+    exclude_files = settings["frame_extraction"]["exclude_files"]
 
-    exclude_dirs = set(fe_settings["exclude_dirs"])
-    exclude_files = fe_settings["exclude_files"]
+    log_dir = settings["frame_extraction"]["log_dir"]
 
-    ffmpeg_filter_str = f'fps={fe_settings["fps"]},lenscorrection=cx={dc_settings["cx"]}:cy={dc_settings["cy"]}:k1={dc_settings["k1"]}:k2={dc_settings["k2"]}:i=bilinear'
-    ffmpeg_args_str = (
-        f"-vf '{ffmpeg_filter_str}' -q:v 1"  # -q:v 1 sets the jpeg quality
-    )
+    fps = settings["frame_extraction"]["fps"]
+    FFMPEG = FFMPEG_frame_extractor(fps, settings["distortion_correction"])
 
     start = datetime.now()
     output_path = os.path.join(output_folder, start.strftime("%Y%m%d-%H%M%S"))
-    log_path = os.path.join(
-        output_folder, fe_settings["log_dir"], start.strftime("%Y%m%d-%H%M%S")
-    )
+    log_path = os.path.join(output_folder, log_dir, start.strftime("%Y%m%d-%H%M%S"))
     pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
     pathlib.Path(log_path).mkdir(parents=True, exist_ok=True)
+
     log_file = os.path.join(log_path, "process_log.txt")
     with open(log_file, "w") as myfile:
         start_str = start.strftime("%d/%m/%Y %H:%M:%S")
@@ -63,21 +147,28 @@ def main(input_folder: str, output_folder: str):
     # Walk through input_folder recursively
     for dirpath, _, filenames in os.walk(input_folder):
         if dirpath.split(sep="/")[-1] in exclude_dirs:
+            # Skip folders in exclude_dirs config
             continue
         for file in filenames:
             if any(excl_file in file for excl_file in exclude_files):
+                # Skip files in exclude_files config
                 continue
             if file.endswith(".mp4"):
                 file_path = os.path.join(dirpath, file)
                 filename = file.replace(".mp4", "")
+                # Generate output paths for FFMPEG
                 ffmpeg_out_path = os.path.join(output_path, filename)
                 ffmpeg_log_file = os.path.join(log_path, f"{filename}.ffmpeg_log.txt")
-                ffmpeg_cmd = f"ffmpeg -i '{file_path}' {ffmpeg_args_str} '{ffmpeg_out_path}_%04d.jpg' 2> '{ffmpeg_log_file}'"
+                # Create full FFMPEG command
+                ffmpeg_cmd = FFMPEG.create_command(
+                    file_path, ffmpeg_out_path, ffmpeg_log_file
+                )
 
                 with open(log_file, "a") as myfile:
                     myfile.write(f"{ffmpeg_cmd}\n")
                 print(ffmpeg_cmd)
 
+                # Run FFMPEG command
                 os.system(ffmpeg_cmd)  # nosec B605
 
     with open(log_file, "a") as myfile:
@@ -86,17 +177,10 @@ def main(input_folder: str, output_folder: str):
 
 
 def parse_args():
-    # setup arg parser
     parser = argparse.ArgumentParser()
-
-    # add arguments
     parser.add_argument("--input_folder", dest="input_folder", type=str)
     parser.add_argument("--output_folder", dest="output_folder", type=str)
-
-    # parse args
     args = parser.parse_args()
-
-    # return args
     return args
 
 
--- objectherkenning_openbare_ruimte/frame_extraction/metadata_helper.py
+++ objectherkenning_openbare_ruimte/frame_extraction/metadata_helper.py
@@ -20,36 +20,52 @@ ObjectherkenningOpenbareRuimteSettings.set_from_yaml(config_path)
 settings = ObjectherkenningOpenbareRuimteSettings.get_settings()
 
 
-def extract_velotech_metadata_fps(metadata_file: str, fps: float) -> pd.DataFrame:
+def extract_velotech_metadata_fps(metadata_file: str, output_file: str, fps: float):
+    """
+    Take a Velotech metadata file as input, and filter it to match a given FPS.
+
+    Timestamps are used to determine which frames should be kept to stay as close
+    as possible to the way in which FFMPEG extracts frames from a video file. In
+    the end, this will always be somewhat of an approximation.
+
+    Parameters:
+    metadata_file: str
+        Original metadata file (assumed to be CSV)
+    output_file: str
+        Where the filtered metadata will be written as CSV
+    fps: float
+        The desired FPS
+    """
     meta_df = pd.read_csv(metadata_file)
 
     # Get timestamps and set start to zero
     timestamps = meta_df["timestamp"].to_numpy()
     timestamps = timestamps - timestamps[0]
 
-    # Change time unit to match fps
+    # Change time unit to match fps rate
     timestamps *= fps
-
-    # FFMPEG extracts the frames mid interval
+    # FFMPEG extracts the frames mid-interval. This means frame one will be after the first half interval.
     first = np.where(timestamps >= 0.5)[0][0]
-
-    # Round time to units and discard first half frame
+    # Round time to units, discard first half interval, and reset start at zero
     ts_rnd = np.round(timestamps[first:] - timestamps[first])
 
-    # Get indices of unique frame points
+    # Get indices of frames at whole time units (to match FPS)
     _, indices = np.unique(ts_rnd, return_index=True)
     indices = indices + first
     if indices[-1] >= len(timestamps):
         indices = indices[:-1]
 
-    return meta_df.iloc[indices, :]
+    # Filter metadata and write output
+    meta_df = meta_df.iloc[indices, :]
+    meta_df["new_frame_id"] = np.arange(1, len(meta_df) + 1)
+    meta_df.to_csv(output_file, index=False)
 
 
 def process_metadata(input_folder: str, output_folder: str):
     """
-    Recursively iterate through all metadata CSV files in the input_folder and
-    its subfolders and extract rows at a given framerate. All resulting metadata
-    files are extracted to the same folder, duplicating the names the source files.
+    Recursively iterate through all metadata CSV files in the input_folder and its subfolders,
+    and filter metadata to match given framerate. Results will be written to the output_folder,
+    duplicating the names the source files.
 
     Usage:
     python metadata_helper.py --input_folder <INPUT> --output_folder <OUTPUT>
@@ -57,47 +73,42 @@ def process_metadata(input_folder: str, output_folder: str):
     Parameters
     ----------
     input_folder: str
-        Folder containing CSV files to be processed.
+        Base folder of input data
     output_folder: str
-        Folder to which extracted metadata files will be written.
+        Output folder for filtered metadata CSVs
     """
-    fe_settings = settings["frame_extraction"]
-    exclude_dirs = set(fe_settings["exclude_dirs"])
-    exclude_files = fe_settings["exclude_files"]
+    exclude_dirs = set(settings["frame_extraction"]["exclude_dirs"])
+    exclude_files = settings["frame_extraction"]["exclude_files"]
+    fps = settings["frame_extraction"]["fps"]
 
     pathlib.Path(output_folder).mkdir(exist_ok=True, parents=True)
 
     # Walk through input_folder recursively
     for dirpath, _, filenames in os.walk(input_folder):
         if dirpath.split(sep="/")[-1] in exclude_dirs:
+            # Skip folders in exclude_dirs config
             continue
         for file in filenames:
             if any(excl_file in file for excl_file in exclude_files):
+                # Skip files in exclude_files config
                 continue
             if file.endswith(".csv"):
                 file_path = os.path.join(dirpath, file)
                 print(f"Processing {file_path}")
-                meta_df = extract_velotech_metadata_fps(
-                    metadata_file=file_path, fps=fe_settings["fps"]
-                )
-                meta_df["new_frame_id"] = np.arange(1, len(meta_df) + 1)
                 out_file_path = os.path.join(output_folder, file)
-                meta_df.to_csv(out_file_path, index=False)
+                extract_velotech_metadata_fps(
+                    metadata_file=file_path,
+                    output_file=out_file_path,
+                    fps=fps,
+                )
                 print(f"Output saved in {out_file_path}")
 
 
 def parse_args():
-    # setup arg parser
     parser = argparse.ArgumentParser()
-
-    # add arguments
     parser.add_argument("--input_folder", dest="input_folder", type=str)
     parser.add_argument("--output_folder", dest="output_folder", type=str)
-
-    # parse args
     args = parser.parse_args()
-
-    # return args
     return args
 
 
