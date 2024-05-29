97751336ad27648f05637011ef222b60e51f438f
Sebastian Davrieux <s.davrieux@amsterdam.nl>
2024-05-29 16:58:24 +0200

    data detection wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#bash objectherkenning_openbare_ruimte/preprocessing_service/preprocess_video_to_frames.sh &
-
 #PYTHONPATH=. poetry run python -u objectherkenning_openbare_ruimte/performance_monitoring/run_performance_monitoring.py &
 PYTHONPATH=. poetry run python -u objectherkenning_openbare_ruimte/detection_pipeline/run_detection_pipeline.py &
 #PYTHONPATH=. poetry run python -u objectherkenning_openbare_ruimte/data_delivery_pipeline/run_data_delivery_pipeline.py &
--- objectherkenning_openbare_ruimte/detection_pipeline/components/data_detection.py
+++ objectherkenning_openbare_ruimte/detection_pipeline/components/data_detection.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 import os
 import pathlib
@@ -6,9 +7,9 @@ from typing import Dict, List
 import cv2
 import numpy as np
 import torch
-from cvtoolkit.helpers.file_helpers import IMG_FORMATS, delete_file
+from cvtoolkit.helpers.file_helpers import delete_file
 from ultralytics import YOLO
-from ultralytics.engine.results import Boxes, Results
+from ultralytics.engine.results import Boxes
 
 from objectherkenning_openbare_ruimte.detection_pipeline.components import (
     blurring_tools,
@@ -68,90 +69,57 @@ class DataDetection:
             - deletes the raw images.
         """
         logger.info(f"Running container detection pipeline on {self.images_folder}..")
-        videos_and_frames = self._find_image_paths_and_group_by_videoname(
+        metadata_csv_file_paths = self._get_frame_metadata_csv_file_paths(
             root_folder=self.images_folder
         )
-        logger.info(
-            f"Number of images to detect: {sum(len(frames) for frames in videos_and_frames.values())}"
-        )
-        self._detect_containers(videos_and_frames=videos_and_frames)
-        self._delete_data(videos_and_frames=videos_and_frames)
+        logger.info(f"Number of CSVs to detect: {len(metadata_csv_file_paths)}")
+        self._detect_and_blur(metadata_csv_file_paths=metadata_csv_file_paths)
+        # self._delete_data(videos_and_frames=videos_and_frames)
 
     @staticmethod
-    def _find_image_paths_and_group_by_videoname(root_folder):
-        videos_and_frames = {}
+    def _get_frame_metadata_csv_file_paths(root_folder):
+        csvs = []
         for foldername, subfolders, filenames in os.walk(root_folder):
             for filename in filenames:
-                if any(filename.endswith(ext) for ext in IMG_FORMATS):
-                    video_name, _ = os.path.basename(filename).rsplit("_frame_", 1)
-                    image_path = os.path.join(foldername, filename)
-                    if video_name not in videos_and_frames:
-                        videos_and_frames[video_name] = [image_path]
-                    else:
-                        videos_and_frames[video_name].append(image_path)
-        return videos_and_frames
+                if (
+                    filename.endswith("csv")
+                    and filename != "runs.csv"
+                    and filename != "system_metrics.csv"
+                ):
+                    filepath = os.path.join(foldername, filename)
+                    csvs.append(filepath)
+        return csvs
 
-    @staticmethod
-    def _delete_data(videos_and_frames: Dict[str, List[str]]):
-        """
-        Deletes the data that has been processed.
+    def _detect_and_blur(self, metadata_csv_file_paths):
+        for metadata_csv_file_path in metadata_csv_file_paths:
+            logger.debug(f"metadata_csv_file_path: {metadata_csv_file_path}")
+            csv_path = pathlib.Path(metadata_csv_file_path)
+            relative_path = csv_path.relative_to(self.images_folder)
+            images_path = pathlib.Path(self.images_folder) / relative_path.parent
 
-        Parameters
-        ----------
-        videos_and_frames
-            List containing the paths of the images to delete.
-        """
-        batch_count = 0
-        for video_name, images_paths in videos_and_frames.items():
-            for image_path in images_paths:
-                delete_file(image_path)
-                batch_count += 1
-        logger.info(f"Number of images deleted: {batch_count}")
-
-    def _detect_containers(self, videos_and_frames: Dict[str, List[str]]):
-        results = self._process_batches(self.model, videos_and_frames, batch_size=1)
-        logger.info(results)
-
-    def _process_batches(self, model, videos_and_frames, batch_size):
-        for video_name, images_paths in videos_and_frames.items():
-            self.inference_params["source"] = images_paths
-            processed_images = 0
-            for i in range(0, len(images_paths), batch_size):
-                try:
-                    batch_images = [
-                        cv2.imread(image_path)
-                        for image_path in images_paths[i : i + batch_size]
-                    ]
-                    # batch_images = self._defisheye(batch_images)
-                    self.inference_params["source"] = batch_images
-                    self.inference_params["name"] = video_name + "_batch_"
-                    batch_results = model(**self.inference_params)
-                    logger.debug(f"Result YOLO: {batch_results}")
-                    torch.cuda.empty_cache()  # Clear unused memory
-                    self._process_results(batch_results)
-                    processed_images += len(batch_images)
-                except RuntimeError as e:
-                    if "out of memory" in str(e):
-                        logger.error(
-                            "Out of memory with batch size of {}".format(batch_size)
+            with open(metadata_csv_file_path) as frame_metadata_file:
+                reader = csv.reader(frame_metadata_file)
+                for idx, row in enumerate(reader):
+                    image_file_name = pathlib.Path(f"{csv_path.stem}-{row[1]}.jpg")
+                    image_full_path = images_path / image_file_name
+                    if os.path.isfile(image_full_path):
+                        image = cv2.imread(str(image_full_path))
+                        image = self._defisheye(image)
+                        self.inference_params["source"] = image
+                        self.inference_params["name"] = csv_path.stem
+                        detection_results = self.model(**self.inference_params)
+                        logger.debug(f"Result YOLO: {detection_results}")
+                        torch.cuda.empty_cache()  # Clear unused memory
+                        image_detection_path = (
+                            pathlib.Path(self.detections_folder) / relative_path.parent
                         )
-                        if batch_size > 1:
-                            new_batch_size = batch_size // 2
-                            logger.warning(
-                                "Trying smaller batch size: {}".format(new_batch_size)
-                            )
-                            return self._process_batches(
-                                model, images_paths, new_batch_size
-                            )
-                        else:
-                            raise RuntimeError(
-                                "Out of memory with the smallest batch size"
-                            )
-                    else:
-                        raise e
-            logger.info(f"Number of images processed: {processed_images}")
+                        self._process_results(
+                            detection_results,
+                            str(image_detection_path),
+                            image_file_name,
+                        )
 
-    def _defisheye(self, batch_images):
+    def _defisheye(self, image):
         if self.roi is None or self.mapx is None or self.mapy is None:
             newcameramtx, self.roi = cv2.getOptimalNewCameraMatrix(
                 np.array(self.defisheye_params["camera_matrix"]),
@@ -169,17 +137,14 @@ class DataDetection:
                 5,
             )
 
-        undistorted_batch_images = []
-        for img in batch_images:
-            # undistort
-            img_dst = cv2.remap(img, self.mapx, self.mapy, cv2.INTER_LINEAR)
+        # undistort
+        img_dst = cv2.remap(image, self.mapx, self.mapy, cv2.INTER_LINEAR)
 
-            # crop the image
-            x, y, w, h = self.roi
-            img_dst = img_dst[y : y + h, x : x + w]
+        # crop the image
+        x, y, w, h = self.roi
+        img_dst = img_dst[y : y + h, x : x + w]
 
-            undistorted_batch_images.append(img_dst)
-        return undistorted_batch_images
+        return img_dst
 
     @staticmethod
     def _get_annotion_string_from_boxes(boxes: Boxes) -> str:
@@ -195,18 +160,20 @@ class DataDetection:
             annotation_lines.append(f"{cls} {yolo_box_str} {conf:.6f} {tracking_id}")
         return "\n".join(annotation_lines)
 
-    def _process_results(self, model_results: Results):
-        for r in model_results:
-            result = r.cpu()
+    def _process_results(
+        self,
+        model_results: List,
+        image_detection_path: str,
+        image_file_name: pathlib.Path,
+    ):
+        for model_result in model_results:
+            result = model_result.cpu()
             boxes = result.boxes.numpy()
 
-            logger.debug(f"Target classes: {self.target_classes}")
-            logger.debug(f"Boxes classes: {boxes.cls}")
-            logger.debug(f"Boxes: {boxes}")
             target_idxs = np.where(np.in1d(boxes.cls, self.target_classes))[0]
             if len(target_idxs) == 0:  # Nothing to do!
-                logger.debug("No container detected.")
-                continue
+                logger.debug("No container detected, not storing the image.")
+                return False
 
             image = result.orig_img.copy()
             sensitive_idxs = np.where(np.in1d(boxes.cls, self.sensitive_classes))[0]
@@ -219,21 +186,36 @@ class DataDetection:
             target_bounding_boxes = boxes[target_idxs].xyxy
             image = blurring_tools.draw_bounding_boxes(image, target_bounding_boxes)
 
-            # Save image
-            folder_path = os.path.join(
-                self.detections_folder, os.path.basename(os.path.dirname(result.path))
-            )
-            logger.debug(f"Folder path: {folder_path}")
-            pathlib.Path(folder_path).mkdir(parents=True, exist_ok=True)
-            logger.debug("Created folder.")
-            save_path = os.path.join(folder_path, pathlib.Path(result.path).name)
-            logger.debug(f"Save path: {save_path}")
-            cv2.imwrite(save_path, image)
+            logger.debug(f"Folder path: {image_detection_path}")
+            pathlib.Path(image_detection_path).mkdir(parents=True, exist_ok=True)
+            logger.debug(f"Save path: {image_detection_path}")
+            result_full_path = os.path.join(image_detection_path, image_file_name)
+            cv2.imwrite(result_full_path, image)
             logger.debug("Saved image.")
 
             # Save annotation
             annotation_str = self._get_annotion_string_from_boxes(boxes[target_idxs])
-            base_name = pathlib.Path(result.path).stem
-            annotation_path = os.path.join(folder_path, f"{base_name}.txt")
+            annotation_path = os.path.join(
+                image_detection_path, f"{image_file_name.stem}.txt"
+            )
             with open(annotation_path, "w") as f:
                 f.write(annotation_str)
+
+            return True
+
+    @staticmethod
+    def _delete_data(videos_and_frames: Dict[str, List[str]]):
+        """
+        Deletes the data that has been processed.
+
+        Parameters
+        ----------
+        videos_and_frames
+            List containing the paths of the images to delete.
+        """
+        batch_count = 0
+        for video_name, images_paths in videos_and_frames.items():
+            for image_path in images_paths:
+                delete_file(image_path)
+                batch_count += 1
+        logger.info(f"Number of images deleted: {batch_count}")
