0a48c166eccd508fa96888f4f82a8e819c52fa2a
epureanudiana <epureanudiana>
2024-06-05 12:27:49 +0000

    submit to signalen step works

--- objectherkenning_openbare_ruimte/post_processing_pipeline/submit_to_signalen.py
+++ objectherkenning_openbare_ruimte/post_processing_pipeline/submit_to_signalen.py
@@ -1,18 +1,15 @@
 from utils_signalen import SignalHandler
 
-LAT = 52.368920318405074  #
-LON = 4.9031888157681935  #
+LAT = 52.368920318405074  
+LON = 4.9031888157681935 
 date_of_notification = "2024-05-24"
+image_to_upload = "/Volumes/dpcv_dev/default/landingzone/test-diana/images/D14M03Y2024/1-0-D14M03Y2024-H13M01S13_0080.jpg"
 
-image_name="test-diana/images/D14M03Y2024/1-0-D14M03Y2024-H13M01S13_0080.jpg"
-dbutils.fs.cp(f"abfss://landingzone@stlandingdpcvontweu01.dfs.core.windows.net/{image_name}", "dbfs:/tmp/{image_name}")
-
-image_to_upload = "/dbfs/tmp/{image_name}"  #
 
 notification_json = SignalHandler.fill_incident_details(incident_date=date_of_notification,
-                                                        lon=LON,
-                                                        lat=LAT,
-                                                        )
+                                                       lon=LON,
+                                                       lat=LAT,
+                                                       )
 
 signalHandler = SignalHandler()
 signalHandler.post_signal_with_image_attachment(json_content=notification_json, filename=image_to_upload)
