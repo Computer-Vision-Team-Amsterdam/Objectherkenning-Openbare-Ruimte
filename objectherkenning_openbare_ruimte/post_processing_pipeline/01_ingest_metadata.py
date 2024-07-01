01621dc88d534ad9ed0baa3cbbf96db651eda7d8
epureanudiana <epureanudiana>
2024-07-01 10:24:05 +0000

    fixed conflicted files

--- objectherkenning_openbare_ruimte/post_processing_pipeline/01_ingest_metadata.py
+++ objectherkenning_openbare_ruimte/post_processing_pipeline/01_ingest_metadata.py
@@ -56,25 +56,14 @@ class DataLoader:
 
         source = f"{self.root_source}/frame_metadata"
         path_table_schema = self._get_schema_path(self.frame_metadata_table)
-<<<<<<< Updated upstream
-        df = self._load_new_frame_metadata(
-            source, path_table_schema=path_table_schema, format="csv"
-        )
-        self._store_new_data(
-            df, checkpoint_path=self.checkpoint_frames, target=self.frame_metadata_table
-        )
-=======
         df = self._load_new_frame_metadata(source, path_table_schema=path_table_schema, format="csv")
         print("Loaded frame metadata.")
         self._store_new_data(df, checkpoint_path=self.checkpoint_frames, target=self.frame_metadata_table)
-         
->>>>>>> Stashed changes
 
     def ingest_detection_metadata(self):
 
         source = f"{self.root_source}/detection_metadata"
         path_table_schema = self._get_schema_path(self.detection_metadata_table)
-<<<<<<< Updated upstream
         df = self._load_new_detection_metadata(
             source, path_table_schema=path_table_schema, format="csv"
         )
@@ -83,13 +72,7 @@ class DataLoader:
             checkpoint_path=self.checkpoint_detections,
             target=self.detection_metadata_table,
         )
-=======
-        df = self._load_new_detection_metadata(source, path_table_schema=path_table_schema, format="csv")
-        print("Loaded detection metadata.")
-        # self._store_new_data(df, checkpoint_path=self.checkpoint_detections, target=self.detection_metadata_table)
-        # print("Stored detection metadata.")
 
->>>>>>> Stashed changes
 
     def _load_new_frame_metadata(
         self, source: str, path_table_schema: str, format: str
@@ -113,21 +96,6 @@ class DataLoader:
             .withColumn("status", lit("Pending"))
         )
 
-<<<<<<< Updated upstream
-=======
-        bronze_df_frame = (self.spark.readStream \
-                .format("cloudFiles") \
-                .option("cloudFiles.format", format) \
-                .option("cloudFiles.schemaLocation", path_table_schema) \
-                .option("cloudFiles.inferColumnTypes", "true") \
-                .option("cloudFiles.schemaHints", 'imu_pitch float, imu_roll float, imu_heading float, imu_gx float, imu_gy float, imu_gz float, gps_lat float, gps_lon float')  
-                .option("cloudFiles.schemaEvolutionMode", "none")   
-                .load(source)
-                .withColumnRenamed("pylon://0_frame_counter", "pylon0_frame_counter")
-                .withColumnRenamed("pylon://0_frame_timestamp", "pylon0_frame_timestamp")
-                .withColumn("status", lit("Pending")))
-        
->>>>>>> Stashed changes
         return bronze_df_frame
 
     def _load_new_detection_metadata(
@@ -154,29 +122,20 @@ class DataLoader:
         stream_query = (
             df.writeStream.option("checkpointLocation", checkpoint_path)
             .trigger(availableNow=True)
-<<<<<<< Updated upstream
-            .toTable(target)
-        )
-
-        # query = f"SELECT COUNT(*) FROM {target}"
-        # query_result = spark.sql(query)
-        # display(query_result)
-        # print(f"Stored {query_result.count()} new rows in {target}.")
-=======
             .toTable(target))
             
 
-        query_progress = stream_query.awaitTermination()  # Wait for 60 seconds (or adjust as needed)
+        # query_progress = stream_query.awaitTermination()  # Wait for 60 seconds (or adjust as needed)
     
-        # Get number of rows processed
-        if query_progress:
-            rows_processed = stream_query.lastProgress["numInputRows"]
-            print(f"Number of rows ingested: {rows_processed}")
-        else:
-            print("Query did not terminate properly.")
->>>>>>> Stashed changes
+        # # Get number of rows processed
+        # if query_progress:
+        #     rows_processed = stream_query.lastProgress["numInputRows"]
+        #     print(f"Number of rows ingested: {rows_processed}")
+        # else:
+        #     print("Query did not terminate properly.")
 
-        stream_query.stop()
+
+        # stream_query.stop()
 
 
 if __name__ == "__main__":
--- objectherkenning_openbare_ruimte/post_processing_pipeline/example_usage.py
+++ objectherkenning_openbare_ruimte/post_processing_pipeline/example_usage.py
@@ -124,16 +124,6 @@ def generate_map(
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Example").getOrCreate()
 
-<<<<<<< Updated upstream
-=======
-def decos():
-    pass
-
-
-if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Example").getOrCreate()
-
->>>>>>> Stashed changes
     # Setup permit data
     az_tenant_id = "72fca1b1-2c2e-4376-a445-294d80196804"
     db_host = "dev-bbn1-01-dbhost.postgres.database.azure.com"
@@ -143,7 +133,6 @@ if __name__ == "__main__":
     query = "SELECT id, kenmerk, locatie, objecten FROM vergunningen_werk_en_vervoer_op_straat WHERE datum_object_van <= '2024-02-17' AND datum_object_tm >= '2024-02-17'"
     decosDataHandler.run(query)
     query_result_df = decosDataHandler.get_query_result_df()
-<<<<<<< Updated upstream
     decosDataHandler.process_query_result()
     healthy_df = decosDataHandler.get_healthy_df()
     coords_geom = decosDataHandler.get_permits_coordinates_geometry()
@@ -178,21 +167,3 @@ if __name__ == "__main__":
         detections=coords_containers_points,
         name="Decos containers",
     )
-
-    
-=======
-    display(query_result_df)
-    # decosDataHandler.process_query_result()
-    # healthy_df = decosDataHandler.get_healthy_df()
-    # display(healthy_df)
-    # coords_geom = decosDataHandler.get_permits_coordinates_geometry()
-    # print(coords_geom)
-
-    # Setup bridges data
-    # root_source = f"abfss://landingzone@stlandingdpcvontweu01.dfs.core.windows.net"
-    # vuln_bridges_rel_path = "test-diana/vuln_bridges.geojson"
-    # file_path = f"{root_source}/{vuln_bridges_rel_path}"
-    # bridgesHandler = VulnerableBridgesHandler(spark, file_path)
-    # bridges_coordinates_geometry = bridgesHandler.get_bridges_coordinates_geometry()
-    # print(bridges_coordinates_geometry)
->>>>>>> Stashed changes
