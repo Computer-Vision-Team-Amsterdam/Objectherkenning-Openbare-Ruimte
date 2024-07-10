8c070a40aa8cc8b6531713bf49809cc6185a517c
epureanudiana <epureanudiana>
2024-07-10 12:14:43 +0000

    disable ssl warnings and print some spaces

--- objectherkenning_openbare_ruimte/post_processing_pipeline/04_submit_to_signalen.py
+++ objectherkenning_openbare_ruimte/post_processing_pipeline/04_submit_to_signalen.py
@@ -6,6 +6,8 @@ from helpers.utils_signalen import SignalHandler
 from helpers.databricks_workspace import get_catalog_name
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+import requests
+requests.packages.urllib3.disable_warnings() 
 
 
 def main():
@@ -37,33 +39,30 @@ def main():
       try:
          # Check if image exists
          dbutils.fs.head(image_upload_path)
-         notification_json = SignalHandler.fill_incident_details(incident_date=date_of_notification,
-                                                       lon=LON,
-                                                       lat=LAT,
-                                                       )
+         notification_json = SignalHandler.fill_incident_details(incident_date=date_of_notification, lon=LON, lat=LAT,)
          id = signalHandler.post_signal_with_image_attachment(json_content=notification_json, filename=image_upload_path)
-         print(f"Created signal {id} with image on {date_of_notification} with lat {LAT} and lon {LON}." )
+         print(f"Created signal {id} with image on {date_of_notification} with lat {LAT} and lon {LON}.\n\n" )
          successful_notifications.append(entry)
       except Exception as e:
          if 'java.io.FileNotFoundException' in str(e):
-            print(f"Image not found: {image_upload_path}. Skip creating notification...")
+            print(f"Image not found: {image_upload_path}. Skip creating notification...\n\n")
             unsuccessful_notifications.append(entry)
 
          else:
-            print(f"An error occurred: {e}")
+            print(f"An error occurred: {e}\n\n")
             unsuccessful_notifications.append(entry)
-
+   
    if successful_notifications:
       successful_df = spark.createDataFrame(successful_notifications, schema=top_scores_df.schema) 
       successful_df.write.mode('append').saveAsTable(f'{signalHandler.catalog_name}.oor.gold_signal_notifications')
       print(f"04: Appended {len(successful_notifications)} rows to gold_signal_notifications.")
    else:
       print("Appended 0 rows to gold_signal_notifications.")
-
+   
    if unsuccessful_notifications:
       unsuccessful_df = spark.createDataFrame(unsuccessful_notifications, schema=top_scores_df.schema)
       print(f"{unsuccessful_df.count()} unsuccessful notifications.")
-
+   
    signalHandler.update_status(table_name="silver_objects_per_day") 
 
 if __name__ == "__main__":
