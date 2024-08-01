7594a52c273408cf46a59ffb3786a9efcfff5a2e
epureanudiana <epureanudiana13@gmail.com>
2024-08-01 10:49:54 +0200

    reformatting feedback pipeline

--- objectherkenning_openbare_ruimte/signalen_feedback_pipeline/01_update_signalen_feedback.py
+++ objectherkenning_openbare_ruimte/signalen_feedback_pipeline/01_update_signalen_feedback.py
@@ -1,69 +1,120 @@
 # this fixes the caching issues, reimports all modules
-dbutils.library.restartPython()
-import os 
-import sys
-sys.path.append('..')
+dbutils.library.restartPython()  # type: ignore[name-defined] # noqa: F821
+import sys  # noqa: E402
 
-from datetime import datetime
-from pyspark.sql.types import StructType
-from post_processing_pipeline.helpers.utils_signalen import SignalHandler
-from post_processing_pipeline.helpers.databricks_workspace import get_catalog_name, set_job_process_time
-from post_processing_pipeline.helpers.table_manager import TableManager
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
-import requests
+sys.path.append("..")
 
-requests.packages.urllib3.disable_warnings() 
+from datetime import datetime  # noqa: E402
 
-#{'text': 'Dit betreft geen container.', 'user': 's.moring@amsterdam.nl', 'state': 'o', //'state_display': 'Afgehandeld', 'target_api': None, 'extra_properties': None, 'send_email': True, 'created_at': '2024-07-16T14:21:04.722426+02:00', 'email_override': None}
+from helpers.databricks_workspace import get_databricks_environment  # noqa: E402
+from post_processing_pipeline.helpers.table_manager import TableManager  # noqa: E402
+from post_processing_pipeline.helpers.utils_signalen import SignalHandler  # noqa: E402
+from pyspark.sql import SparkSession  # noqa: E402
+from pyspark.sql.types import StructType  # noqa: E402
 
-# {'text': 'Morgen word alles opgeruimd en afgehandeld.', 'user': 'p.herrebrugh@amsterdam.nl', 'state': 'o', 'state_display': 'Afgehandeld', 'target_api': None, 'extra_properties': None, 'send_email': True, 'created_at': '2024-07-18T12:54:06.659573+02:00', 'email_override': None}
+from objectherkenning_openbare_ruimte.settings.databricks_jobs_settings import (  # noqa: E402
+    load_settings,
+)
 
-   
+
+def run_update_signalen_feedback_step(
+    sparkSession,
+    catalog,
+    schema,
+    client_id,
+    client_secret_name,
+    access_token_url,
+    base_url,
+    job_process_time,
+):
+
+    signalHandler = SignalHandler(
+        sparkSession,
+        catalog,
+        schema,
+        client_id,
+        client_secret_name,
+        access_token_url,
+        base_url,
+    )
+
+    tableManager = TableManager(spark=SparkSession, catalog=catalog, schema=schema)
+    job_process_time = datetime.fromisoformat(job_process_time)
+
+    signalen_feedback_entries = []
+    ids_of_not_updated_status = []
+    for entry in signalHandler.get_pending_signalen_notifications().collect():
+        id = entry["id"]
+        signal_status = signalHandler.get_signal(sig_id=entry["signal_id"])["status"]
+        if signal_status["state_display"] != "Gemeld":
+            print(f"status is {signal_status['state_display']}")
+            print(f"id is {entry['id']}")
+            status, text, user, status_update_time = (
+                signal_status["state_display"],
+                signal_status["text"],
+                signal_status["user"],
+                signal_status["created_at"],
+            )
+            # Convert the date string to a datetime object
+            date_obj = datetime.fromisoformat(status_update_time)
+
+            formatted_date = datetime(
+                year=date_obj.year,
+                month=date_obj.month,
+                day=date_obj.day,
+                hour=date_obj.hour,
+                minute=date_obj.minute,
+                second=date_obj.second,
+            )
+            print(type(job_process_time))
+            print(f"job time is: {job_process_time}")
+            table_entry = (entry["signal_id"], status, text, user, formatted_date)
+            signalen_feedback_entries.append(table_entry)
+        else:
+            ids_of_not_updated_status.append(id)
+
+    signalen_feedback_df = signalHandler.get_signalen_feedback()
+    filtered_schema = StructType(
+        [
+            field
+            for field in signalen_feedback_df.schema.fields
+            if field.name not in {"id", "processed_at"}
+        ]
+    )
+
+    print(signalen_feedback_entries)
+    signalen_feedback_df = spark.createDataFrame(  # noqa: F821
+        signalen_feedback_entries, filtered_schema
+    )
+    display(signalen_feedback_df)  # noqa: F821
+    signalen_feedback_df.write.mode("append").saveAsTable(
+        f"{signalHandler.catalog_name}.oor.bronze_signal_notifications_feedback"
+    )
+    print(
+        f"01: Appended {len(signalen_feedback_entries)} rows to bronze_signal_notifications_feedback."
+    )
+
+    print(ids_of_not_updated_status)
+    tableManager.update_status(
+        table_name="gold_signal_notifications",
+        job_process_time=job_process_time,
+        exclude_ids=ids_of_not_updated_status,
+    )
+
 
 if __name__ == "__main__":
-   sparkSession = SparkSession.builder.appName("SignalFeedback").getOrCreate()
-   signalHandler = SignalHandler(sparkSession)
-   tableManager = TableManager(sparkSession, catalog=signalHandler.catalog_name)
-   job_process_time = set_job_process_time()
-   job_process_time = datetime.fromisoformat(job_process_time)
-
-   signalen_feedback_entries = []
-   ids_of_not_updated_status = []
-   for entry in signalHandler.get_pending_signalen_notifications().collect():
-      id = entry["id"]
-      signal_status = signalHandler.get_signal(sig_id=entry["signal_id"])["status"]
-      if signal_status["state_display"] != "Gemeld":
-         print(f"status is {signal_status['state_display']}")
-         print(f"id is {entry['id']}")
-         status, text, user, status_update_time = signal_status["state_display"], signal_status["text"], signal_status["user"], signal_status["created_at"]
-         # Convert the date string to a datetime object
-         date_obj = datetime.fromisoformat(status_update_time)
-
-         formatted_date= datetime(
-                  year=date_obj.year,
-                  month=date_obj.month,
-                  day=date_obj.day,
-                  hour=date_obj.hour,
-                  minute=date_obj.minute,
-                  second=date_obj.second
-               )
-         print(type(job_process_time))  
-         print(f"job time is: {job_process_time}")             
-         table_entry = (entry["signal_id"], status, text, user, formatted_date)
-         signalen_feedback_entries.append(table_entry)
-      else:
-         ids_of_not_updated_status.append(id)
-   
-   signalen_feedback_df = signalHandler.get_signalen_feedback() 
-   filtered_schema = StructType([field for field in signalen_feedback_df.schema.fields if field.name not in {'id', 'processed_at'}])
-
-   print(signalen_feedback_entries)
-   signalen_feedback_df = spark.createDataFrame(signalen_feedback_entries, filtered_schema) 
-   display(signalen_feedback_df)
-   signalen_feedback_df.write.mode('append').saveAsTable(f'{signalHandler.catalog_name}.oor.bronze_signal_notifications_feedback')
-   print(f"01: Appended {len(signalen_feedback_entries)} rows to bronze_signal_notifications_feedback.")
-
-   print(ids_of_not_updated_status)
-   tableManager.update_status(table_name="gold_signal_notifications", job_process_time=job_process_time, exclude_ids=ids_of_not_updated_status)
-   
+    sparkSession = SparkSession.builder.appName("SignalenFeedback").getOrCreate()
+    databricks_environment = get_databricks_environment(sparkSession)
+    settings = load_settings("../../config.yml")["databricks_pipelines"][
+        f"{databricks_environment}"
+    ]
+    run_update_signalen_feedback_step(
+        sparkSession=sparkSession,
+        catalog=settings["catalog"],
+        schema=settings["schema"],
+        client_id=settings["signalen"]["client_id"],
+        client_secret_name=settings["signalen"]["client_secret_name"],
+        access_token_url=settings["signalen"]["access_token_url"],
+        base_url=settings["signalen"]["base_url"],
+        job_process_time="2024-07-30 13:00:00",
+    )
