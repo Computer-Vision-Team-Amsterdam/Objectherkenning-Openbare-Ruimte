d951c4e7727dcfe9db16de09ded0f2c7d616c78a
epureanudiana <epureanudiana13@gmail.com>
2024-08-01 14:48:08 +0200

    fix paths 4th step

--- objectherkenning_openbare_ruimte/databricks_pipelines/post_processing_pipeline/submit_to_signalen_step/run_submit_to_signalen_step.py
+++ objectherkenning_openbare_ruimte/databricks_pipelines/post_processing_pipeline/submit_to_signalen_step/run_submit_to_signalen_step.py
@@ -1,14 +1,18 @@
 # this fixes the caching issues, reimports all modules
 dbutils.library.restartPython()  # type: ignore[name-defined] # noqa: F821
 
-from helpers.utils_signalen import SignalHandler  # noqa: E402
+import os  # noqa: E402
+
 from pyspark.sql import SparkSession  # noqa: E402
 
-from objectherkenning_openbare_ruimte.post_processing_pipeline.databricks_workspace import (  # noqa: E402
+from objectherkenning_openbare_ruimte.databricks_pipelines.common.databricks_workspace import (  # noqa: E402
     get_databricks_environment,
 )
-from objectherkenning_openbare_ruimte.post_processing_pipeline.table_manager import (  # noqa: E402
+from objectherkenning_openbare_ruimte.databricks_pipelines.common.table_manager import (  # noqa: E402
     TableManager,
+)
+from objectherkenning_openbare_ruimte.databricks_pipelines.common.utils_signalen import (  # noqa: E402
+    SignalHandler,
 )
 from objectherkenning_openbare_ruimte.settings.databricks_jobs_settings import (  # noqa: E402
     load_settings,
@@ -70,7 +74,11 @@ def run_submit_to_signalen_step(
 if __name__ == "__main__":
     sparkSession = SparkSession.builder.appName("SignalHandler").getOrCreate()
     databricks_environment = get_databricks_environment(sparkSession)
-    settings = load_settings("../../config.yml")["databricks_pipelines"][
+    project_root = os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
+    )
+    config_file_path = os.path.join(project_root, "config.yml")
+    settings = load_settings(config_file_path)["databricks_pipelines"][
         f"{databricks_environment}"
     ]
     run_submit_to_signalen_step(
