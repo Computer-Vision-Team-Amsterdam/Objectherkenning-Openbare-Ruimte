1362e86251ded03e6778afa22b2cb5b22f3ab4d7
epureanudiana <epureanudiana13@gmail.com>
2024-08-01 14:39:05 +0200

    fix paths in 2nd step

--- objectherkenning_openbare_ruimte/databricks_pipelines/post_processing_pipeline/metadata_healthcheck_step/run_metadata_healthcheck_step.py
+++ objectherkenning_openbare_ruimte/databricks_pipelines/post_processing_pipeline/metadata_healthcheck_step/run_metadata_healthcheck_step.py
@@ -1,12 +1,14 @@
 # this fixes the caching issues, reimports all modules
 dbutils.library.restartPython()  # type: ignore[name-defined] # noqa: F821
 
+import os  # noqa: E402
+
 from pyspark.sql import SparkSession  # noqa: E402
 
-from objectherkenning_openbare_ruimte.post_processing_pipeline.databricks_workspace import (  # noqa: E402
+from objectherkenning_openbare_ruimte.databricks_pipelines.common.databricks_workspace import (  # noqa: E402
     get_databricks_environment,
 )
-from objectherkenning_openbare_ruimte.post_processing_pipeline.helpers.metadata_healthcheck import (  # noqa: E402
+from objectherkenning_openbare_ruimte.databricks_pipelines.post_processing_pipeline.metadata_healthcheck_step.components.metadata_healthcheck import (  # noqa: E402
     MetadataHealthChecker,
 )
 from objectherkenning_openbare_ruimte.settings.databricks_jobs_settings import (  # noqa: E402
@@ -20,8 +22,12 @@ def run_metadata_healthcheck_step(sparkSession, catalog, schema, job_process_tim
 
 if __name__ == "__main__":
     sparkSession = SparkSession.builder.appName("MetadataHealthChecker").getOrCreate()
+    project_root = os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
+    )
+    config_file_path = os.path.join(project_root, "config.yml")
     databricks_environment = get_databricks_environment(sparkSession)
-    settings = load_settings("../../config.yml")["databricks_pipelines"][
+    settings = load_settings(config_file_path)["databricks_pipelines"][
         f"{databricks_environment}"
     ]
     run_metadata_healthcheck_step(
