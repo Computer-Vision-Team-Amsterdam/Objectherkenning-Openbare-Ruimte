fdbd857da63bd8785440507a9f73aef5a5e5652f
epureanudiana <epureanudiana13@gmail.com>
2024-09-17 15:24:27 +0200

    removed obsolete function

--- objectherkenning_openbare_ruimte/databricks_pipelines/common/tables/silver/objects.py
+++ objectherkenning_openbare_ruimte/databricks_pipelines/common/tables/silver/objects.py
@@ -17,20 +17,6 @@ class SilverObjectsPerDayManager(TableManager):
         super().__init__(spark, catalog, schema, table_name)
 
     def get_top_pending_records(self, limit=20):
-        # Select all rows where status is 'Pending' and detections are containers, sort by score in descending order, and limit the results to the top 10
-        select_query = f"""
-        SELECT * FROM {self.catalog}.{self.schema}.{self.table_name}
-        WHERE status = 'Pending' AND object_class = 2 AND score >= 0.4
-        ORDER BY score DESC
-        LIMIT {limit}
-        """  # nosec
-        results = self.spark.sql(select_query)
-        print(
-            f"Loaded {results.count()} rows with top {limit} scores from {self.catalog}.{self.schema}.{self.table_name}."
-        )
-        return results
-
-    def get_top_pending_records_no_sql(self, limit=20):
         table_full_name = f"{self.catalog}.{self.schema}.{self.table_name}"
         results = (
             self.spark.table(table_full_name)
