ad077c8100ebc4c248622f65982a565046e4180d
epureanudiana <epureanudiana>
2024-09-24 14:25:37 +0000

    stop debugging

--- objectherkenning_openbare_ruimte/databricks_pipelines/post_processing_pipeline/data_enrichment_step/run_data_enrichment_step.py
+++ objectherkenning_openbare_ruimte/databricks_pipelines/post_processing_pipeline/data_enrichment_step/run_data_enrichment_step.py
@@ -85,7 +85,7 @@ def run_data_enrichment_step(
         db_port=5432,
     )
 
-    print(f"03: Number of containers: {containers_coordinates_df.count()}.")
+    print(f"Number of containers: {containers_coordinates_df.count()}.")
 
     # Enrich with bridges data
     closest_bridges_df = (
@@ -102,9 +102,7 @@ def run_data_enrichment_step(
     )
 
     # Enrich with decos data
-    # date_to_query = datetime.today().strftime("%Y-%m-%d")
-    # date_to_query = job_process_time.strftime("%Y-%m-%d")
-    date_to_query = "2024-08-27"
+    date_to_query = datetime.today().strftime("%Y-%m-%d")
     query = f"SELECT id, kenmerk, locatie, objecten FROM vergunningen_werk_en_vervoer_op_straat WHERE datum_object_van <= '{date_to_query}' AND datum_object_tm >= '{date_to_query}'"  # nosec B608
     print(f"Querying the database for date {date_to_query}...")
     decosDataHandler.run(query)
@@ -136,31 +134,30 @@ def run_data_enrichment_step(
     display(containers_coordinates_with_closest_bridge_and_closest_permit_and_score_df)
     display(clustering.joined_metadata)
 
-    clustering.joined_metadata = clustering.joined_metadata.drop("gps_lat", "gps_lon")
-    print("dropped gps from joined metadata")
-    display(clustering.joined_metadata)
+    
     joined_metadata_with_closest_bridge_and_closest_permit_and_score_df = containers_coordinates_with_closest_bridge_and_closest_permit_and_score_df.alias(
         "a"
     ).join(
         clustering.joined_metadata.alias("b"),
         on=F.col("a.detection_id") == F.col("b.detection_id"),
     )
+    
 
     print("After join")
     display(joined_metadata_with_closest_bridge_and_closest_permit_and_score_df)
-    # # Gather data to visualize
-    # utils_visualization.generate_map(
-    #     dataframe=joined_metadata_with_closest_bridge_and_closest_permit_and_score_df,
-    #     name=f"{job_process_time}-map",
-    #     path=f"/Volumes/{catalog}/default/landingzone/Luna/visualizations/{date_to_query}/",
-    # )
+    # Gather data to visualize
+    utils_visualization.generate_map(
+        dataframe=joined_metadata_with_closest_bridge_and_closest_permit_and_score_df,
+        name=f"{job_process_time}-map",
+        path=f"/Volumes/{catalog}/default/landingzone/Luna/visualizations/{date_to_query}/",
+    )
 
     selected_casted_df = (
         joined_metadata_with_closest_bridge_and_closest_permit_and_score_df.select(
             F.col("a.detection_id").cast("int"),
             F.col("object_class"),
-            F.col("a.gps_lat").alias("object_lat").cast("string"),
-            F.col("a.gps_lon").alias("object_lon").cast("string"),
+            F.col("b.gps_lat").alias("object_lat").cast("string"),
+            F.col("b.gps_lon").alias("object_lon").cast("string"),
             F.col("closest_bridge_distance")
             .alias("distance_closest_bridge")
             .cast("float"),
