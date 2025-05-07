62371b3f9f864b5cedf77cd01e3f28ed02ffc728
daanbl <daanbl>
2025-05-07 09:00:00 +0000

    Add stadsdelen query and handler

--- objectherkenning_openbare_ruimte/databricks_pipelines/post_processing_pipeline/submit_to_signalen_step/components/terrain_data_connector.py
+++ objectherkenning_openbare_ruimte/databricks_pipelines/post_processing_pipeline/submit_to_signalen_step/components/terrain_data_connector.py
@@ -1,7 +1,8 @@
 from typing import Any, List, Optional
 
 import pandas as pd
-from shapely import wkb
+import requests
+from shapely import Polygon, wkb
 
 from objectherkenning_openbare_ruimte.databricks_pipelines.common.reference_db_connector import (  # noqa: E402
     ReferenceDatabaseConnector,
@@ -40,6 +41,34 @@ class TerrainDatabaseConnector(ReferenceDatabaseConnector):
         result_df = result_df[result_df["polygon"].notnull()]
         public_terrains = result_df.to_dict(orient="records")
         return public_terrains
+
+    def query_and_process_stadsdelen(self) -> Optional[dict]:
+        """
+        Query the database for stadsdelen geometries and process the results.
+
+        The function runs a query to fetch geometries for stadsdelen and converts them
+        from WKB hex strings to Shapely geometry objects.
+        """
+        url = "https://api.data.amsterdam.nl/v1/gebieden/stadsdelen/"
+        print("Querying stadsdelen API...")
+        try:
+            result = requests.get(url)
+            result.raise_for_status()
+            stadsdelen_dict: dict[str, List[dict]] = {
+                "stadsdelen": []
+            }
+            for stadsdeel in result.json()["_embedded"]["stadsdelen"]:
+                stadsdelen_dict["stadsdelen"].append(
+                    {
+                        "naam": stadsdeel["naam"],
+                        "code": stadsdeel["code"],
+                        "polygon": Polygon(stadsdeel["geometrie"]["coordinates"][0]),
+                    }
+                )
+            return stadsdelen_dict
+        except requests.exceptions.RequestException as e:
+            print(f"Error querying stadsdelen API: {e}")
+            return None
 
     def convert_wkb(self, hex_str: str) -> Optional[Any]:
         """
